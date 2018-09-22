24be92fe34ec1a74c9d7523ba8ab497696767634
niksaz <nikitasazanovich@gmail.com>
2018-09-22 23:15:57 +0300

    Added Visdom loggers instead of print()s.

--- 2/homework/test.py
+++ 2/homework/test.py
@@ -1,15 +1,24 @@
+""" To run the test:
+        $ python -m visdom.server -port 8097 &
+        $ python test.py
+"""
+
 import torchvision.datasets as datasets
 import torchvision.transforms as transforms
 import torch.nn as nn
 import torch.optim as optim
+import torchnet as tnt
 import numpy as np
 import torch
 import random
 import os.path
 
+from tqdm import tqdm
 from torch.utils.data import DataLoader
 from torch.utils.data import Sampler
-from destinynets.resnext import resnext18
+from torchnet.engine import Engine
+from torchnet.logger import VisdomPlotLogger
+from destinynets.resnext import resnext50
 
 
 def set_random_seed(seed=1234):
@@ -37,68 +46,86 @@ def load_datasets(root, transform):
     return train_set, test_set
 
 
-class SequentialPrefixSampler(Sampler):
+class RandomPrefixSampler(Sampler):
 
     def __init__(self, data_source, prefix):
-        super(SequentialPrefixSampler, self).__init__(data_source)
+        super(RandomPrefixSampler, self).__init__(data_source)
         self.prefix = min(prefix, len(data_source))
 
     def __iter__(self):
-        return iter(range(self.prefix))
+        return iter(random.sample(range(self.prefix), self.prefix))
 
     def __len__(self):
         return self.prefix
 
 
 def dataset_to_loader(dataset, prefix, batch_size=10):
-    prefix_sampler = SequentialPrefixSampler(dataset, prefix=prefix)
+    prefix_sampler = RandomPrefixSampler(dataset, prefix=prefix)
     dataloader = DataLoader(dataset, batch_size=batch_size,
                             sampler=prefix_sampler)
     return dataloader
 
 
-def train_and_evaluate(train_dataloader, test_dataloader):
+def train_and_track(train_dataloader, test_dataloader):
     criterion = nn.CrossEntropyLoss()
-    model = resnext18()
+
+    model = resnext50()
     learning_rate = 0.001
     optimizer = optim.Adam(model.parameters(), lr=learning_rate)
 
-    num_epochs = 5
+    engine = Engine()
 
-    for epoch in range(num_epochs):
-        model.train()
+    port = 8097
+    train_loss_logger = VisdomPlotLogger(
+        'line', port=port, opts={'title': 'Train CrossEntropyLoss'})
+    train_err_logger = VisdomPlotLogger(
+        'line', port=port, opts={'title': 'Train Class Accuracy'})
+    test_loss_logger = VisdomPlotLogger(
+        'line', port=port, opts={'title': 'Test CrossEntropyLoss'})
+    test_err_logger = VisdomPlotLogger(
+        'line', port=port, opts={'title': 'Test Class Accuracy'})
 
-        for images, labels in train_dataloader:
-            outputs = model(images)
-            loss = criterion(outputs, labels)
+    meter_loss = tnt.meter.AverageValueMeter()
+    classerr = tnt.meter.ClassErrorMeter(accuracy=True)
 
-            optimizer.zero_grad()
-            loss.backward()
-            optimizer.step()
+    def run_model(sample):
+        images, labels = sample
+        outputs = model(images)
+        loss = criterion(outputs, labels)
+        return loss, outputs
 
-        model.eval()
+    def reset_meters():
+        classerr.reset()
+        meter_loss.reset()
 
-        correct = 0
-        for images, labels in test_dataloader:
-            outputs = model(images)
+    def on_forward(state):
+        classerr.add(state['output'].data, torch.LongTensor(state['sample'][1]))
+        meter_loss.add(state['loss'].data.item())
 
-            _, predicted_labels = torch.max(outputs.data, dim=1)
-            correct += (predicted_labels == labels).sum()
+    def on_start_epoch(state):
+        reset_meters()
+        state['iterator'] = tqdm(state['iterator'])
 
-        accuracy = 100 * correct / len(test_dataloader.sampler)
+    def on_end_epoch(state):
+        train_loss_logger.log(state['epoch'], meter_loss.value()[0])
+        train_err_logger.log(state['epoch'], classerr.value()[0])
 
-        print('After epoch', epoch)
-        print('Loss =', loss.data.item())
-        print('Accuracy =', accuracy.item())
-        print()
+        # Check accuracy on test after each epoch.
+        reset_meters()
+        engine.test(run_model, test_dataloader)
+        test_loss_logger.log(state['epoch'], meter_loss.value()[0])
+        test_err_logger.log(state['epoch'], classerr.value()[0])
+
+    engine.hooks['on_forward'] = on_forward
+    engine.hooks['on_start_epoch'] = on_start_epoch
+    engine.hooks['on_end_epoch'] = on_end_epoch
+    engine.train(run_model, train_dataloader, maxepoch=10, optimizer=optimizer)
 
 
 def main():
     set_random_seed()
 
     root = create_root_for_data()
-    print('Data root is', root)
-    print()
 
     train_set, test_set = load_datasets(
         root,
@@ -107,14 +134,10 @@ def main():
         ])
     )
 
-    train_dataloader = dataset_to_loader(train_set, prefix=1000)
+    train_dataloader = dataset_to_loader(train_set, prefix=500)
     test_dataloader = dataset_to_loader(test_set, prefix=500)
 
-    print('len(train_dataloader) is', len(train_dataloader.sampler))
-    print('len(test_dataloader) is', len(test_dataloader.sampler))
-    print()
-
-    train_and_evaluate(train_dataloader, test_dataloader)
+    train_and_track(train_dataloader, test_dataloader)
 
 
 if __name__ == "__main__":
